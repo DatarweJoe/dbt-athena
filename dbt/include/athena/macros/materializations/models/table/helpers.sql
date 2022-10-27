ca8211ae8d170be2ccf79902557b46cce721c1d2
Joe Burton <joe.burton@datarwe.com>
2022-10-27 21:27:14 +1000

    clean-up macro code

--- dbt/include/athena/macros/materializations/models/table/helpers.sql
+++ dbt/include/athena/macros/materializations/models/table/helpers.sql
@@ -1,6 +1,6 @@
-{% macro materialize_table(format, old_relation, target_relation, sql) -%}
-    {%- if old_relation -%}
-        {{ adapter.drop_relation(old_relation) }}
+{% macro materialize_table(format, existing_relation, target_relation, sql) -%}
+    {%- if existing_relation is not none -%}
+        {{ adapter.drop_relation(existing_relation) }}
     {%- endif -%}
 
     -- Generate and execute build SQL
@@ -13,19 +13,10 @@
     {{ set_table_classification(target_relation, format) }}
 {%- endmacro %}
 
-{% macro materialize_table_iceberg(format, old_relation, target_relation, tmp_relation, sql) -%}
-    -- If iceberg is True, ensure that a valid format is provided.
-    {% set invalid_iceberg_format_msg -%}
-        Invalid format provided for iceberg table: {{ format }}
-        Expected one of: 'parquet'
-    {%- endset %}
-    {%- if format != 'parquet' -%}
-        {% do exceptions.raise_compiler_error(invalid_iceberg_format_msg) %}
-    {%- endif -%}
+{% macro materialize_table_iceberg(format, existing_relation, target_relation, sql) -%}
+    {{ validate_format_iceberg(format) }}
 
-    {%- set tmp_relation = make_temp_relation(target_relation) -%}
-
-    {%- set build_sql = create_table_iceberg(target_relation, old_relation, tmp_relation, sql) -%}
+    {%- set build_sql = create_table_iceberg(existing_relation, target_relation, sql) -%}
 
     {% call statement("main") %}
       {{ build_sql }}
